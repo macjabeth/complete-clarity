554a7577abcd2dd83f7a7b73d0b190715503c793
Jonathan Picazo <macbethjonathan@gmail.com>
2021-10-01 19:31:01 -0500

    ♻️ Update theme configuration

--- theme.config.js
+++ theme.config.js
@@ -1,10 +1,10 @@
 export default {
-  github: 'https://github.com/macjabeth/complete-clarity',
-  docsRepositoryBase: 'https://github.com/macjabeth/complete-clarity/blob/master',
-  titleSuffix: ' – Complete Clarity',
+  github: 'https://github.com/macjabeth/lucidity',
+  docsRepositoryBase: 'https://github.com/macjabeth/lucidity/blob/master',
+  titleSuffix: ' – L U C I D I T Y',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Complete Clarity</span>
+      <span className="mr-2 font-extrabold hidden md:inline">L U C I D I T Y</span>
       <span className="text-gray-600 font-normal hidden md:inline">New World Company</span>
     </>
   ),
@@ -14,15 +14,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Complete Clarity: New World company" />
-      <meta name="og:description" content="Complete Clarity: New World company" />
+      <meta name="description" content="L U C I D I T Y: New World company" />
+      <meta name="og:description" content="L U C I D I T Y: New World company" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://complete-clarity.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="complete-clarity.vercel.app" />
-      <meta name="twitter:url" content="https://complete-clarity.vercel.app" />
-      <meta name="og:title" content="Complete Clarity: New World Company" />
-      <meta name="og:image" content="https://complete-clarity.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Complete Clarity" />
+      <meta name="twitter:image" content="https://lucidity.vercel.app/og.png" />
+      <meta name="twitter:site:domain" content="lucidity.vercel.app" />
+      <meta name="twitter:url" content="https://lucidity.vercel.app" />
+      <meta name="og:title" content="L U C I D I T Y: New World Company" />
+      <meta name="og:image" content="https://lucidity.vercel.app/og.png" />
+      <meta name="apple-mobile-web-app-title" content="L U C I D I T Y" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
